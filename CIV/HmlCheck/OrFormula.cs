1b466bc0099a1ea9a207c5feacad7ae40c415d02
Luca Di Stefano <luca.distefano@gssi.it>
2017-05-27 02:02:06 +0200

    Short-circuit logical operators

--- CIV/HmlCheck/OrFormula.cs
+++ CIV/HmlCheck/OrFormula.cs
@@ -8,6 +8,6 @@ namespace CIV.HmlCheck
 		public IHmlFormula Inner1 { get; set; }
 		public IHmlFormula Inner2 { get; set; }
         public bool Check(IProcess process) => 
-            Inner1.Check(process) | Inner2.Check(process);
+            Inner1.Check(process) || Inner2.Check(process);
     }
 }
