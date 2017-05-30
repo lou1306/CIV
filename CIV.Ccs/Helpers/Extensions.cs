ac7db55bbb7f349f745b4865730b35d158fc47d0
Luca Di Stefano <luca.distefano@gssi.it>
2017-05-30 14:53:08 +0200

    Move WeakTransitions to CcsProcess
    Add IEquatable<> interface to CcsProcess

--- CIV.Ccs/Helpers/Extensions.cs
+++ CIV.Ccs/Helpers/Extensions.cs
@@ -29,30 +29,5 @@ namespace CIV.Ccs
         {
             return action != Const.tau && action[0] == '\'';
         }
-    
-        /// <summary>
-        /// Returns weak transitions for the given process.
-        /// Weak transitions:
-        /// <list type="number">
-        /// <item>Have a non-tau label;</item>
-        /// <item>Can be reached directly or with any number of tau moves.</item>
-        /// </list>
-        /// </summary>
-        /// <returns>The weak transitions.</returns>
-        /// <param name="process">A process.</param>
-        public static IEnumerable<Transition> WeakTransitions(this IProcess process)
-        {
-            var transitions = process.Transitions();
-            var result = (
-                from t in transitions
-                where t.Label != Const.tau
-                select t
-            );
-            foreach (var t in transitions.Where(x => x.Label == Const.tau))
-            {
-                result.Concat(t.Process.WeakTransitions());
-            }
-            return result;
-        }
     }
 }
--- CIV.Ccs/Processes/CcsProcess.cs
+++ CIV.Ccs/Processes/CcsProcess.cs
@@ -1,27 +1,32 @@
-﻿using System.Linq;
+﻿using System;
 using System.Collections.Generic;
 using CIV.Interfaces;
-         
+
 namespace CIV.Ccs
 {
-    public abstract class CcsProcess : IHasWeakTransitions
+    public abstract class CcsProcess : IHasWeakTransitions, IEquatable<CcsProcess>
     {
+        public abstract bool Equals(CcsProcess other);
+
         public abstract IEnumerable<Transition> Transitions();
 
-		public IEnumerable<Transition> WeakTransitions()
-		{
-			var transitions = Transitions();
-			var result = (
-				from t in transitions
-				where t.Label != Const.tau
-				select t
-			);
-			foreach (var t in transitions.Where(x => x.Label == Const.tau))
-			{
-				result.Concat(t.Process.WeakTransitions());
-			}
-			return result;
-		}
-
+        public IEnumerable<Transition> WeakTransitions()
+        {
+            // https://stackoverflow.com/questions/3969963/when-not-to-use-yield-return
+            var stack = new Queue<Transition>(Transitions());
+            while (stack.Count > 0)
+            {
+                var t = stack.Dequeue();
+                yield return t;
+                if (t.Label == Const.tau)
+                {
+                    foreach (var t1 in t.Process.Transitions())
+                    {
+                        stack.Enqueue(t1);
+                        //System.Console.WriteLine("Queueing {0}", t1.Label);
+                    }
+                }
+            }
+        }
     }
 }
