5738f12bc11550dd3c8605d9beb03c71abc1b07a
Luca Di Stefano <luca.distefano@gssi.it>
2017-05-30 14:53:57 +0200

    Make NilProcess singleton

--- CIV.Ccs/Processes/NilProcess.cs
+++ CIV.Ccs/Processes/NilProcess.cs
@@ -4,11 +4,21 @@ using CIV.Interfaces;
 
 namespace CIV.Ccs
 {
+    /// <summary>
+    /// Nil process. Implemented as a singleton class.
+    /// </summary>
     class NilProcess : CcsProcess
     {
+        public static NilProcess Instance { get; } = new NilProcess();
+
+        NilProcess(){}
+
         public override IEnumerable<Transition> Transitions()
         {
             return Enumerable.Empty<Transition>();
         }
+        public override bool Equals(CcsProcess other) => other is NilProcess;
+
+        public override int GetHashCode() => 1;
     }
 }
