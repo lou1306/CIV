773f8d6e8ec38b28b7d5aae5fc724ff329abb495
Luca Di Stefano <luca.distefano@gssi.it>
2017-05-31 11:25:22 +0200

    Add PidProcess class

---
+++ CIV.Ccs/Processes/PidProcess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CIV.Interfaces;
+
+namespace CIV.Ccs
+{
+    public class PidProcess : CcsProcess
+    {
+        public CcsProcess Inner { get; set; }
+        public string Pid { get; set; }
+
+        public override bool Equals(CcsProcess other)
+        {
+            var otherPid = other as PidProcess;
+            if (otherPid != null)
+            {
+                return Pid == otherPid.Pid;
+            }
+            return Inner.Equals(other);
+        }
+
+        public override IEnumerable<Transition> GetTransitions() => Inner.GetTransitions();
+
+        public override string ToString() => Pid;
+    }
+}
