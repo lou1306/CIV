e1c8ed9f496e08e2c3980fdaee5f264822ec7540
Luca Di Stefano <luca.distefano@gssi.it>
2017-05-31 11:28:00 +0200

    Add new members to Const class
    Now Const acts as a facade to the CcsLexer class.
    This allows us to never use string literals and to
    adapt to changes in the lexer (which hopefully should never
    happen, now)

--- CIV.Ccs/Const.cs
+++ CIV.Ccs/Const.cs
@@ -1,7 +1,30 @@
-﻿namespace CIV.Ccs
+﻿using System;
+namespace CIV.Ccs
 {
-	public static class Const
-	{
-		public const string tau = "tau";
-	}
+    public static class Const
+    {
+        public static readonly string tau = GetLiteral(CcsLexer.TAU);
+        public static readonly string nil = GetLiteral(CcsLexer.NIL);
+		public static readonly string par = GetLiteral(CcsLexer.PAR);
+		public static readonly string prefix = GetLiteral(CcsLexer.PREFIX);
+        public static readonly string choice = GetLiteral(CcsLexer.CHOICE);
+        public static readonly string restrictFormat =
+            String.Format(
+                "{{0}}{0}{1}{{1}}{2}",
+                GetLiteral(CcsLexer.T__1),
+                GetLiteral(CcsLexer.LBRACE),
+                GetLiteral(CcsLexer.RBRACE));
+        public static readonly string relabelFormat =
+			String.Format(
+				"{{0}}{0}{{1}}{1}",
+                GetLiteral(CcsLexer.LBRACK),
+				GetLiteral(CcsLexer.RBRACK));
+
+        static string GetLiteral(int id)
+        {
+            return CcsLexer.DefaultVocabulary
+                           .GetLiteralName(id)
+                           .Replace("'", "");
+        }
+    }
 }
