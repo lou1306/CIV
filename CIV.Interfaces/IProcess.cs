7e0318ce690303183a38229a578f49d409902302
Luca Di Stefano <luca.distefano@gssi.it>
2017-05-31 15:06:50 +0200

    Add EqualityComparers for IProcess and Transition

--- CIV.Interfaces/IProcess.cs
+++ CIV.Interfaces/IProcess.cs
@@ -5,5 +5,20 @@ namespace CIV.Interfaces
     public interface IProcess
     {
         IEnumerable<Transition> GetTransitions();
+
+        string ToString();
+    }
+
+    public class ProcessComparer : IEqualityComparer<IProcess>
+    {
+        public bool Equals(IProcess x, IProcess y)
+        {
+            return x.ToString() == y.ToString();
+        }
+
+        public int GetHashCode(IProcess obj)
+        {
+            return obj.ToString().GetHashCode();
+        }
     }
 }
