ca57fde52ac11081ba56c2e22bfe403c9c483873
Luca Di Stefano <luca.distefano@gssi.it>
2017-06-02 15:34:41 +0200

    Read Caal file in Main

--- CIV/Program.cs
+++ CIV/Program.cs
@@ -1,6 +1,6 @@
 ﻿﻿using static System.Console;
+using CIV.Formats;
 using CIV.Ccs;
-using CIV.Hml;
 
 namespace CIV
 {
@@ -10,10 +10,23 @@ namespace CIV
         {
   			var text = System.IO.File.ReadAllText(args[0]);
 
-			var processes = CcsFacade.ParseAll(text);
-			var hmlText = "[[ack]][[ack]][[ack]](<<ack>>tt and [[freeAll]]ff)";
-			var prova = HmlFacade.ParseAll(hmlText);
-			WriteLine(prova.Check(processes["Prison"]));
-		}
+			//var processes = CcsFacade.ParseAll(text);
+			//var hmlText = "[[ack]][[ack]][[ack]](<<ack>>tt and [[freeAll]]ff)";
+			//var prova = HmlFacade.ParseAll(hmlText);
+			//WriteLine(prova.Check(processes["Prison"]));
+
+            var project = new Caal().Load(args[0]);
+            WriteLine("Loaded project {0}", project.Name);
+			WriteLine("Processes:");
+            //foreach (var kv in project.Processes)
+            //{
+            //    WriteLine("{0} = {1}", kv.Key, (kv.Value as PidProcess).Inner);
+            //}
+            foreach (var kv in project.Formulae)
+            {
+                WriteLine($"Result for formula {kv.Value}:");
+                WriteLine(kv.Value.Check(project.Processes[kv.Key]));
+            }
+        }
     }
 }
