4345c7d0e873712b343aef18513f65a440bcb356
Luca Di Stefano <luca.distefano@gssi.it>
2017-06-09 16:06:59 +0200

    Better exit codes in Main

--- CIV/Program.cs
+++ CIV/Program.cs
@@ -8,12 +8,13 @@ namespace CIV
 {
 
 	[Flags]
-	enum ExitCodes : int
+	enum ExitCode
 	{
 		Success = 0,
-		FileNotFound = 1,
-        ParsingFailed = 2,
-        VerificationFailed = 4
+        UnknownError = 1,
+		FileNotFound = 2,
+        ParsingFailed = 4,
+        VerificationFailed = 8,
 	}
 
 
@@ -24,38 +25,58 @@ namespace CIV
             try
             {
 				var project = new Caal().Load(args[0]);
-                VerifyAll(project);
-			}
+                var result = VerifyAll(project);
+                if (!result)
+                {
+                    Environment.Exit((int)ExitCode.VerificationFailed);
+                }
+            }
             catch (Exception ex)
             {
                 ForegroundColor = ConsoleColor.Red;
                 WriteLine("An error has occurred:");
                 WriteLine(ex.Message);
 				ResetColor();
-                Environment.Exit((int)ExitCodes.FileNotFound);
+
+                ExitCode exitCode;
+                switch (ex)
+                {
+                    case System.IO.FileNotFoundException e:
+                        exitCode = ExitCode.FileNotFound;
+                        break;
+                    case ParsingFailedException e:
+                        exitCode = ExitCode.ParsingFailed;
+                        break;
+                    default:
+                        exitCode = ExitCode.UnknownError;
+                        break;
+                }
+                Environment.Exit((int)exitCode);
 			}
 		}
 
-        static void VerifyAll(Caal project)
+        static bool VerifyAll(Caal project)
         {
 			WriteLine("Loaded project {0}. Starting verification...", project.Name);
 
             var sw = new Stopwatch();
             sw.Start();
+
+            bool result = true;
 			foreach (var kv in project.Formulae)
 			{
                 Write($"{kv.Value} |= {kv.Key}...");
 				Out.Flush();
 				var isSatisfied = kv.Key.Check(kv.Value);
+                result &= isSatisfied;
 				ForegroundColor = isSatisfied ? ConsoleColor.Green : ConsoleColor.Red;
-                var result = isSatisfied ? "Success!" : "Failure";
-                Write($"\t{result}");
+                Write(isSatisfied ? "Success!" : "Failure");
                 WriteLine();
 				ResetColor();
 			}
             sw.Stop();
             WriteLine($"Completed in {sw.Elapsed.TotalMilliseconds} ms.");
-
+            return result;
         }
     }
 }
