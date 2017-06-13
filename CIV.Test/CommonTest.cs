e1bd39a3745d00a3612093e07bd98bdc6bd01a11
Luca Di Stefano <luca.distefano@gssi.it>
2017-06-13 10:19:06 +0200

    Add TopSet generic class & tests

---
+++ CIV.Test/CommonTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using CIV.Common;
+
+namespace CIV.Test
+{
+    public class CommonTest
+    {
+        [Fact]
+        public void TopSetContainsEveryElement()
+        {
+            var top = new TopSet<string>();
+            Assert.True(top.Contains("action"));
+
+        }
+
+        [Fact]
+        public void TopSetContainsEverySet()
+        {
+			var top = new TopSet<string>();
+			var s = new HashSet<string> { "action" };
+			Assert.True(top.IsSupersetOf(s));
+            Assert.True(top.Overlaps(s));
+			Assert.True(top.IsProperSupersetOf(s));
+			Assert.False(top.IsSubsetOf(s));
+			Assert.False(top.IsProperSubsetOf(s));
+        }
+
+    }
+}
